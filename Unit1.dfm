2cdbce803483301ecee6c4960b819b7afd538873
MuhmmdKoko <someanncat@gmail.com>
2023-07-11 10:48:28 +0800

    membuat fungsi main menu pada form pertama

--- Unit1.dfm
+++ Unit1.dfm
@@ -10,7 +10,62 @@ object Form1: TForm1
   Font.Height = -11
   Font.Name = 'Tahoma'
   Font.Style = []
+  Menu = mm1
   OldCreateOrder = False
   PixelsPerInch = 96
   TextHeight = 13
+  object mm1: TMainMenu
+    Left = 24
+    Top = 24
+    object N1: TMenuItem
+      Caption = 'tambah data'
+      object N2as1: TMenuItem
+        Caption = 'form siswa'
+        OnClick = N2as1Click
+      end
+      object formortu1: TMenuItem
+        Caption = 'form ortu'
+        OnClick = formortu1Click
+      end
+      object formwalikelas1: TMenuItem
+        Caption = 'form wali kelas'
+        OnClick = formwalikelas1Click
+      end
+      object form1: TMenuItem
+        Caption = 'form kelas'
+        OnClick = form1Click
+      end
+      object formpoin1: TMenuItem
+        Caption = 'form poin'
+        OnClick = formpoin1Click
+      end
+      object formriwayatpoin1: TMenuItem
+        Caption = 'form riwayat poin'
+        OnClick = formriwayatpoin1Click
+      end
+      object formuser1: TMenuItem
+        Caption = 'form user'
+        OnClick = formuser1Click
+      end
+      object formhubungan1: TMenuItem
+        Caption = 'form hubungan'
+        OnClick = formhubungan1Click
+      end
+    end
+    object formjoin1: TMenuItem
+      Caption = 'form join'
+      object innerjoin1: TMenuItem
+        Caption = 'inner join'
+      end
+      object outerjoin1: TMenuItem
+        Caption = 'cross join'
+      end
+      object rightjoin1: TMenuItem
+        Caption = 'right join'
+      end
+      object leftjoin1: TMenuItem
+        Caption = 'left join'
+      end
+    end
+  end
 end
--- Unit1.pas
+++ Unit1.pas
@@ -4,10 +4,33 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs;
+  Dialogs, Menus;
 
 type
   TForm1 = class(TForm)
+    mm1: TMainMenu;
+    N1: TMenuItem;
+    N2as1: TMenuItem;
+    formortu1: TMenuItem;
+    formwalikelas1: TMenuItem;
+    form1: TMenuItem;
+    formpoin1: TMenuItem;
+    formriwayatpoin1: TMenuItem;
+    formuser1: TMenuItem;
+    formhubungan1: TMenuItem;
+    formjoin1: TMenuItem;
+    innerjoin1: TMenuItem;
+    outerjoin1: TMenuItem;
+    rightjoin1: TMenuItem;
+    leftjoin1: TMenuItem;
+    procedure N2as1Click(Sender: TObject);
+    procedure formortu1Click(Sender: TObject);
+    procedure formwalikelas1Click(Sender: TObject);
+    procedure form1Click(Sender: TObject);
+    procedure formhubungan1Click(Sender: TObject);
+    procedure formpoin1Click(Sender: TObject);
+    procedure formriwayatpoin1Click(Sender: TObject);
+    procedure formuser1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -19,7 +42,49 @@ var
 
 implementation
 
+uses Unit2, Unit3, Unit4, Unit5, Unit6, Unit7, Unit8, Unit9;
+
 {$R *.dfm}
+
+procedure TForm1.N2as1Click(Sender: TObject);
+begin
+form2.showmodal;
+end;
+
+procedure TForm1.formortu1Click(Sender: TObject);
+begin
+form3.showmodal;
+end;
+
+procedure TForm1.formwalikelas1Click(Sender: TObject);
+begin
+form4.showmodal;
+end;
+
+procedure TForm1.form1Click(Sender: TObject);
+begin
+form5.showmodal
+end;
+
+procedure TForm1.formhubungan1Click(Sender: TObject);
+begin
+form6.showmodal;
+end;
+
+procedure TForm1.formpoin1Click(Sender: TObject);
+begin
+form7.showmodal;
+end;
+
+procedure TForm1.formriwayatpoin1Click(Sender: TObject);
+begin
+form8.showmodal;
+end;
+
+procedure TForm1.formuser1Click(Sender: TObject);
+begin
+form9.showmodal;
+end;
 
 end.
  
